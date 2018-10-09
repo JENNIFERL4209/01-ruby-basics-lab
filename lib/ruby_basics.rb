733d90f229bb23d116eab8578d121707235353d5
JENNIFER LIU <jenniferl4209@hstat.org>
2018-10-09 18:32:18 +0000

    define methods with parameters

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,23 +1,25 @@
-def division
-    
+def division(num1=14, num2=2)
+    num1/num2
 end
 
-def argue
-  
+def argue(argument)
+    argument = "I'm right and you are wrong!"
+
 end
 
-def greeting
+def greeting(greeting, name)
 
 end
 
 def return_a_value
+    return "Nice"
     
 end
 
-def pizza_party
-    
+def pizza_party(cheese="cheese")
+    cheese
 end
 
-def whisper
-    
+def whisper(something)
+    something.downcase
 end
